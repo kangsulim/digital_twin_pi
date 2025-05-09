d8314a1cc7c7402670d56eb1fed3e62e655d81db
kangsulim <rkdtnfla1@naver.com>
2025-05-09 10:45:05 +0900

    exam

--- pi_study/exam/password.py
+++ pi_study/exam/password.py
@@ -38,7 +38,7 @@ inputValue3 = gpio.HIGH
 
 def button1():
     global inputPassword, inputValue1
-    if inputValue1 == gpio.input(buttons[0].pin):
+    if inputValue1 == gpio.input(buttons[0].pin) and inputValue1 == gpio.HIGH:
         inputPassword.append(1)
         inputValue1 = gpio.LOW
     elif gpio.input(buttons[0].pin) == gpio.LOW:
@@ -46,7 +46,7 @@ def button1():
 
 def button2():
     global inputPasswordm, inputValue2
-    if inputValue2 == gpio.input(buttons[1].pin):
+    if inputValue2 == gpio.input(buttons[1].pin) and inputValue1 == gpio.HIGH:
         inputPassword.append(2)
         inputValue2 = gpio.LOW
     elif gpio.input(buttons[1].pin) == gpio.LOW:
@@ -54,7 +54,7 @@ def button2():
 
 def button3():
     global inputPassword, inputValue3
-    if inputValue3 == gpio.input(buttons[2].pin):
+    if inputValue3 == gpio.input(buttons[2].pin) and inputValue1 == gpio.HIGH:
         inputPassword.append(3)
         inputValue3 = gpio.LOW
     elif gpio.input(buttons[2].pin) == gpio.LOW:
